51373b8ee392b44d2c8774e9cd39244656866285
songbinliu <songbin.liu@turbonomic.com>
2018-02-12 00:19:38 -0500

    nits

--- client/run.sh
+++ client/run.sh
@@ -4,7 +4,7 @@ hostTomcat=http://127.0.0.1:8080
 hostApache=http://127.0.0.1:9191
 
 #1. connect direct
-./echoClient -serverHost $hostTomcat
+./echoClient --serverHost $hostTomcat
 
 #2. connect via proxy
-./echoClient -serverHost $hostApache
+./echoClient --serverHost $hostApache
